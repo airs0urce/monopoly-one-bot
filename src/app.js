d70fb5157d0a0b9d1ff3d737f7b0325daaf9322f
Dmitry Eroshenko <airs0urce0@gmail.com>
2020-06-22 12:15:54 +0700

    change

--- src/app.js
+++ src/app.js
@@ -106,7 +106,7 @@ let browser;
         debug(`обработка игроков со стола ${game.title}. Время стола: ${game.timeString}`);
         
         for (let player of game.players) {
-            if (handledProfilesCount >= 17) {
+            if (handledProfilesCount >= 20) {
                 // add or remove item from market. Ban protection
                 debug(`${handledProfilesCount} профайлов обработано, удаляем\добавляем вещь на маркет, чтобы избежать банов...`);
                 await addOrRemoveFromMarket(page);
